b09eb0ae709eafc89b8c3d4d033eb5b0f04015fb
caner_ozdemir_cgi <canerozdemir.mail@gmail.com>
2023-08-02 19:30:31 +0300

    Commit 2023.08.02

---
+++ Contents/scripts/ToolSeq_ReNamer.py
@@ -0,0 +1,184 @@
+import itertools
+import re
+from os.path import abspath, dirname
+from sys import stderr
+
+from PySide2.QtCore import QEvent, Qt
+from PySide2.QtGui import QTextCursor
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QWidget, QComboBox, QStyledItemDelegate
+from maya import cmds as maya_cmds
+from maya.OpenMayaUI import MQtUtil
+from shiboken2 import wrapInstance
+
+class ToolSeq_ReNamer(QWidget):
+
+	userScriptDir = dirname(abspath(__file__)) + '/'
+
+	qUiLoader = QUiLoader()
+	qUiLoader.setWorkingDirectory(userScriptDir)
+
+	windowMain = wrapInstance(int(MQtUtil.mainWindow()), QMainWindow)
+
+	def __init__(self):
+		super(ToolSeq_ReNamer, self).__init__(ToolSeq_ReNamer.windowMain)
+
+		self.window = ToolSeq_ReNamer.qUiLoader.load(ToolSeq_ReNamer.userScriptDir + 'ui/ToolSeq_ReNamer.ui', self)
+		self.window.setWindowFlags(self.window.windowFlags() & ~Qt.WindowMinMaxButtonsHint)
+		self.window.setAttribute(Qt.WA_DeleteOnClose)
+		for qComboBox in self.window.findChildren(QComboBox):
+			qComboBox.setItemDelegate(QStyledItemDelegate(qComboBox))
+		with open(ToolSeq_ReNamer.userScriptDir + 'ToolSeq.qss', 'r') as fileStyleSheet:
+			self.window.setStyleSheet(fileStyleSheet.read())
+
+		self.window.Widget_01.installEventFilter(self)
+		self.window.Text_Transforms.textChanged.connect(self.Event_Transforms)
+		self.window.Button_Select.installEventFilter(self)
+		self.window.Button_Rename.installEventFilter(self)
+
+		self.window.show()
+
+	def eventFilter(self, source, event):
+		sourceObjectName = source.objectName()
+		eventType = event.type()
+		if sourceObjectName == 'Widget_01':
+			if eventType == QEvent.MouseButtonRelease:
+				eventButton = event.button()
+				if eventButton == Qt.LeftButton:
+					self.Event_TransformsCount_1()
+				elif eventButton == Qt.RightButton:
+					self.Event_TransformsCount_3()
+				elif eventButton == Qt.MiddleButton:
+					self.Event_TransformsCount_2()
+		elif sourceObjectName == 'Button_Select':
+			if eventType == QEvent.MouseButtonRelease:
+				if event.button() == Qt.LeftButton:
+					self.Action_Select()
+		elif sourceObjectName == 'Button_Rename':
+			if eventType == QEvent.MouseButtonRelease:
+				if event.button() == Qt.LeftButton:
+					self.Action_Rename()
+		return False
+
+	def Event_TransformsCount_1(self):
+		self.window.Text_Transforms.setPlainText('\n'.join(maya_cmds.ls(selection=True, transforms=True)) + '\n')
+
+	def Event_TransformsCount_3(self):
+		self.window.Text_Transforms.moveCursor(QTextCursor.End)
+		self.window.Text_Transforms.insertPlainText('\n'.join(maya_cmds.ls(selection=True, transforms=True)) + '\n')
+
+	def Event_TransformsCount_2(self):
+		self.window.Text_Transforms.clear()
+
+	def Event_Transforms(self):
+		text = self.window.Text_Transforms.toPlainText().strip()
+		self.window.Label_TransformsCount.setText(str(text.count('\n') + 1) if text else '0')
+
+	def Action_Select(self):
+		maya_cmds.undoInfo(openChunk=True)
+		try:
+			transforms = self.window.Text_Transforms.toPlainText().strip().split('\n')
+			searchStr = self.window.Line_Search.text().strip()
+
+			results = self.Activity_Search(transforms, searchStr)
+			maya_cmds.select(results)
+		except Exception as e:
+			print(str(e), file=stderr)
+		maya_cmds.undoInfo(closeChunk=True)
+
+	def Action_Rename(self):
+		maya_cmds.undoInfo(openChunk=True)
+		try:
+			transforms = self.window.Text_Transforms.toPlainText().strip().split('\n')
+			searchStr = self.window.Line_Search.text().strip()
+			if searchStr == '':
+				return
+			replaceStr = self.window.Line_Replace.text().strip()
+
+			transforms = self.Filter_Instances(transforms)
+
+			results = self.Activity_Rename(transforms, searchStr, replaceStr)
+			results.sort(key=lambda s: s.count('|'))
+			results.reverse()
+
+			print('ToolSeq_ReNamer: %s objects will be renamed' % len(results))
+			for filtered, renamed in results:
+				maya_cmds.rename(filtered, renamed)
+				print('ToolSeq_ReNamer: %s => %s' % (filtered, renamed))
+			maya_cmds.confirmDialog(title='ToolSeq_ReNamer', message='%s objects have been renamed' % len(results), button=['OK'])
+			self.Event_TransformsCount_2()
+		except Exception as e:
+			print(str(e), file=stderr)
+			maya_cmds.confirmDialog(title='ToolSeq_ReNamer', message='Unexpected error has been occurred, check Script Editor', button=['OK'])
+		maya_cmds.undoInfo(closeChunk=True)
+
+	def Activity_Search(self, sources, searchStr):
+		if self.window.Check_Regex.isChecked():
+			if self.window.Check_CaseSensitive.isChecked():
+				searchRe = re.compile(searchStr)
+			else:
+				searchRe = re.compile(searchStr, re.IGNORECASE)
+			results = [source for source in sources if searchRe.search(source.split('|')[-1] if '|' in source else source)]
+		else:
+			if self.window.Check_CaseSensitive.isChecked():
+				results = [source for source in sources if searchStr in (source.split('|')[-1] if '|' in source else source)]
+			else:
+				results = [source for source in sources if searchStr.lower() in (source.split('|')[-1].lower() if '|' in source else source.lower())]
+		return results
+
+	def Activity_Rename(self, sources, searchStr, replaceStr):
+		if self.window.Check_Regex.isChecked():
+			if self.window.Check_CaseSensitive.isChecked():
+				searchRe = re.compile(searchStr)
+			else:
+				searchRe = re.compile(searchStr, re.IGNORECASE)
+			results = []
+			for source in sources:
+				sourceSplit = source.split('|')
+				if searchRe.search(sourceSplit[-1] if '|' in source else source):
+					results.append((source, searchRe.sub(replaceStr, sourceSplit[-1])))
+		else:
+			if self.window.Check_CaseSensitive.isChecked():
+				results = []
+				for source in sources:
+					sourceSplit = source.split('|')
+					if searchStr in (sourceSplit[-1] if '|' in source else source):
+						results.append((source, sourceSplit[-1].replace(searchStr, replaceStr)))
+			else:
+				results = []
+				for source in sources:
+					sourceSplit = source.split('|')
+					if searchStr.lower() in (sourceSplit[-1].lower() if '|' in source else source.lower()):
+						results.append((source, ToolSeq_ReNamer.Replace_All_IgnoreCase(sourceSplit[-1], searchStr, replaceStr)))
+		return results
+
+	@staticmethod
+	def Replace_All_IgnoreCase(sourceStr, searchStr, replaceStr):
+		sourceStrLower = sourceStr.lower()
+		searchStrLower = searchStr.lower()
+
+		result = ''
+		indexSta = 0
+
+		while (index := sourceStrLower.find(searchStrLower, indexSta)) != -1:
+			result += sourceStr[indexSta:index] + replaceStr
+			indexSta = index + len(searchStr)
+
+		result += sourceStr[indexSta:]
+		return result
+
+	@staticmethod
+	def Filter_Instances(transforms):
+		result = {}
+		for transform in transforms:
+			if '|' in transform:
+				transformParents = maya_cmds.listRelatives(transform, path=True, allParents=True)
+				if (len(transformParents) if transformParents is not None else 0) > 1:
+					frozensetTransformParents = frozenset(transformParents)
+					if frozensetTransformParents not in result:
+						result[frozensetTransformParents] = maya_cmds.listRelatives(transformParents[0], path=True, children=True)
+				else:
+					result[transform] = [transform]
+			else:
+				result[transform] = [transform]
+		return list(itertools.chain.from_iterable(result.values()))
---
+++ Contents/scripts/ToolSeq_UV.py
@@ -0,0 +1,140 @@
+from os.path import abspath, dirname
+from sys import stderr
+
+from PySide2.QtCore import QEvent, Qt
+from PySide2.QtGui import QTextCursor
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow, QWidget, QComboBox, QStyledItemDelegate
+from maya import cmds as maya_cmds
+from maya.OpenMayaUI import MQtUtil
+from shiboken2 import wrapInstance
+
+class ToolSeq_UV(QWidget):
+
+	userScriptDir = dirname(abspath(__file__)) + '/'
+
+	qUiLoader = QUiLoader()
+	qUiLoader.setWorkingDirectory(userScriptDir)
+
+	windowMain = wrapInstance(int(MQtUtil.mainWindow()), QMainWindow)
+
+	def __init__(self):
+		super(ToolSeq_UV, self).__init__(ToolSeq_UV.windowMain)
+
+		self.window = ToolSeq_UV.qUiLoader.load(ToolSeq_UV.userScriptDir + 'ui/ToolSeq_UV.ui', self)
+		self.window.setWindowFlags(self.window.windowFlags() & ~Qt.WindowMinMaxButtonsHint)
+		self.window.setAttribute(Qt.WA_DeleteOnClose)
+		for qComboBox in self.window.findChildren(QComboBox):
+			qComboBox.setItemDelegate(QStyledItemDelegate(qComboBox))
+		with open(ToolSeq_UV.userScriptDir + 'ToolSeq.qss', 'r') as fileStyleSheet:
+			self.window.setStyleSheet(fileStyleSheet.read())
+
+		self.window.Widget_01.installEventFilter(self)
+		self.window.Widget_012.installEventFilter(self)
+		self.window.Text_Items.textChanged.connect(self.Event_Items)
+		self.window.Combo_CutType.currentTextChanged.connect(self.Event_CutType)
+		self.window.Button_Apply.installEventFilter(self)
+
+		self.window.show()
+		wMax1 = max(self.window.Check_Unfold.width(), self.window.Check_Optimize.width())
+		self.window.Check_Unfold.setFixedWidth(wMax1)
+		self.window.Check_Optimize.setFixedWidth(wMax1)
+		wMax2 = max(self.window.Label_TileSpace.width(), self.window.Label_ShellSpace.width())
+		self.window.Label_TileSpace.setFixedWidth(wMax2)
+		self.window.Label_ShellSpace.setFixedWidth(wMax2)
+
+	def eventFilter(self, source, event):
+		sourceObjectName = source.objectName()
+		eventType = event.type()
+		if sourceObjectName == 'Widget_01':
+			if eventType == QEvent.MouseButtonRelease:
+				eventButton = event.button()
+				if eventButton == Qt.LeftButton:
+					self.Event_ItemsCount_1()
+				elif eventButton == Qt.RightButton:
+					self.Event_ItemsCount_3()
+				elif eventButton == Qt.MiddleButton:
+					self.Event_ItemsCount_2()
+		elif sourceObjectName == 'Widget_012':
+			if eventType == QEvent.MouseButtonRelease:
+				if event.button() == Qt.LeftButton:
+					self.Event_ItemsCoverage()
+					return True
+		elif sourceObjectName == 'Button_Apply':
+			if eventType == QEvent.MouseButtonRelease:
+				if event.button() == Qt.LeftButton:
+					self.Action_Apply()
+		return False
+
+	def Event_ItemsCount_1(self):
+		self.window.Text_Items.setPlainText('\n'.join(maya_cmds.polyListComponentConversion(toFace=True)) + '\n')
+
+	def Event_ItemsCount_3(self):
+		self.window.Text_Items.moveCursor(QTextCursor.End)
+		self.window.Text_Items.insertPlainText('\n'.join(maya_cmds.polyListComponentConversion(toFace=True)) + '\n')
+
+	def Event_ItemsCount_2(self):
+		self.window.Text_Items.clear()
+
+	def Event_Items(self):
+		text = self.window.Text_Items.toPlainText().strip()
+		self.window.Label_ItemsCount.setText(str(text.count('\n') + 1) if text else '0')
+		self.Event_ItemsCoverage()
+
+	def Event_ItemsCoverage(self):
+		text = self.window.Text_Items.toPlainText().strip()
+		self.window.Label_ItemCoverage.setText(' '.join(str(round(100 * i, 5)) + '%' for i in maya_cmds.polyUVCoverage(text.split('\n'))) if text else '0%')
+
+	def Event_CutType(self, text):
+		if text == 'Automatic':
+			self.window.Float_CutTolerance.setVisible(True)
+			self.window.Line_CutName.setVisible(False)
+		elif text == 'Manual':
+			self.window.Float_CutTolerance.setVisible(False)
+			self.window.Line_CutName.setVisible(True)
+
+	def Action_Apply(self):
+		maya_cmds.undoInfo(openChunk=True)
+		try:
+			items = self.window.Text_Items.toPlainText().strip().split('\n')
+
+			if self.window.Check_Unitize.isChecked():
+				maya_cmds.polyForceUV(items, unitize=True)
+				maya_cmds.polyMapSewMove(items, caching=True)
+
+			if self.window.Check_Cut.isChecked():
+				cutType = self.window.Combo_CutType.currentText()
+				if cutType == 'Automatic':
+					maya_cmds.u3dAutoSeam(items, splitShells=self.window.Float_CutTolerance.value())
+				elif cutType == 'Manual':
+					maya_cmds.polyMapCut(maya_cmds.sets(self.window.Line_CutName.text().strip(), q=True))
+
+			if self.window.Check_Unfold.isChecked():
+				maya_cmds.u3dUnfold(items, iterations=self.window.Int_UnfoldIteration.value(), pack=False)
+
+			if self.window.Check_Optimize.isChecked():
+				maya_cmds.u3dOptimize(items, iterations=self.window.Int_OptimizeIteration.value())
+
+			if self.window.Check_Layout.isChecked():
+				paramRotate = {}
+				if self.window.Check_LayoutRotation.isChecked():
+					paramRotate = {
+						'rotateStep': self.window.Float_LayoutRotationStep.value(),
+						'rotateMin': self.window.Float_LayoutRotationMin.value(),
+						'rotateMax': self.window.Float_LayoutRotationMax.value()
+					}
+				maya_cmds.u3dLayout(
+					items,
+					mutations=self.window.Int_LayoutIteration.value(),
+					resolution=self.window.Int_LayoutResolution.value(),
+					preRotateMode=self.window.Combo_LayoutAlign.currentIndex(),
+					preScaleMode=1,
+					tileMargin=self.window.Int_LayoutTileSpaceL.value() / float(self.window.Int_LayoutTileSpaceH.value()),
+					shellSpacing=self.window.Int_LayoutShellSpaceL.value() / float(self.window.Int_LayoutShellSpaceH.value()),
+					**paramRotate
+				)
+
+			self.Event_ItemsCoverage()
+		except Exception as e:
+			print(str(e), file=stderr)
+		maya_cmds.undoInfo(closeChunk=True)
